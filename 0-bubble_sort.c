7d8670210106d4940b021813c65e0cca5e491741
buezman <chibuezenwajiobi@gmail.com>
2023-04-24 14:28:16 -0700

    update bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,18 +7,18 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
+	size_t i, j;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; ++i)
 	{
-		for (i = 0; i < size - i - 1; i++)
+		for (j = 0; j < size - i - 1; ++j)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 			}
 			print_array(array, size);
 		}
