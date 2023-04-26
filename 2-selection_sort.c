0a48bad1a7848280c1e72fca51843c32ae8db998
buezman <chibuezenwajiobi@gmail.com>
2023-04-26 01:25:49 -0700

    add selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * selection_sort - selection sort algorithm
+ * @array: given array to be sorted
+ * @size: size of array
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[i])
+			{
+				min = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
+}
